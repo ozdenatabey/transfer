af88bad069a704594365e275499554e3ed1ce17f
ozden <ozdenatabey@hotmail.com>
2024-11-17 22:19:47 +0300

    Hero v1

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,56 @@
 import React from "react";
 
+import { locations } from "@/data/data";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+
 const Hero = () => {
   return (
-    <div className="relative flex h-screen w-full items-center justify-center bg-white bg-dot-blue-800/[0.5] dark:bg-black dark:bg-dot-cyan-500/[0.5]">
-      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
-      <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-7xl">
-        Debeme
-      </p>
+    <div className="bg-default relative flex h-screen w-full items-center justify-center bg-dot-base dark:bg-dot-primary">
+      <div className="bg-default pointer-events-none absolute inset-0 flex items-center justify-center [mask-image:radial-gradient(ellipse_at_center,transparent,black)] "></div>
+      <div className="border-default shadow-default z-10 flex flex-col gap-3 rounded-2xl border bg-white/0 p-10 backdrop-blur-[3px]">
+        <p className="text-4xl font-bold tracking-wider text-primary md:text-6xl lg:text-8xl">
+          Atabey Transfer
+        </p>
+        <div className="flex flex-col justify-between gap-3 text-lg md:flex-row md:gap-6">
+          <label className="flex-1">
+            <p className="pb-1">From</p>
+            <Select>
+              <SelectTrigger>
+                <SelectValue placeholder="Select" />
+              </SelectTrigger>
+              <SelectContent>
+                {locations.map((location) => (
+                  <SelectItem key={location.value} value={location.value}>
+                    {location.field}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </label>
+          <label className="flex-1">
+            <p className="pb-1">To</p>
+            <Select>
+              <SelectTrigger>
+                <SelectValue placeholder="Select" />
+              </SelectTrigger>
+              <SelectContent>
+                {locations.map((location) => (
+                  <SelectItem key={location.value} value={location.value}>
+                    {location.field}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </label>
+        </div>
+      </div>
     </div>
   );
 };
--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -25,8 +25,8 @@ export function ModeToggle() {
             className="size-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
           />
           <Moon
-            color="#06b6d4"
-            fill="#06b6d4"
+            color="#00ADB5"
+            fill="#00ADB5"
             className="absolute size-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
           />
           <span className="sr-only">Toggle theme</span>
